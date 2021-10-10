49998eef34230953c4856c27ae04fe164d4d4ce5
cecyRdz <92197205+cecyRdz@users.noreply.github.com>
2021-10-10 13:17:33 -0500

    Creación del archivo encargado de la consulta de los productos
    Agregue dos columnas por cada producto que coloque en la lista, una es de editar y otra es de eliminar
    agregue los links, ustedes realicen su direccionamiento

--- controlador/app.py
+++ controlador/app.py
@@ -5,6 +5,10 @@ app = Flask(__name__, template_folder='../vista')
 def inicio():
     return render_template('comunes/Index.html')
 
+@app.route('/consultarProductos')
+def consultarProductos():
+    return render_template('Productos/consultarProductos.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
